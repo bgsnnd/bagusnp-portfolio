fed55a0eaec2728ac591a5382a2c0bd0bb04f5f9
bgsnnd <bgsnnd8@gmail.com>
2025-02-28 16:29:50 +0700

    y

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -3,47 +3,51 @@
 import Link from "next/link";
 import { usePathname } from "next/navigation";
 
-
 const links = [
   {
-    name : "home",
-    path  : "/"
+    name: "home",
+    path: "/",
   },
   {
-    name : "services",
-    path  : "/services"
+    name: "services",
+    path: "/services",
   },
   {
-    name : "resume",
-    path  : "/resume"
+    name: "resume",
+    path: "/resume",
   },
   {
-    name : "work",
-    path  : "/work"
+    name: "work",
+    path: "/work",
   },
   {
-    name : "contact",
-    path  : "/contact"
+    name: "contact",
+    path: "/contact",
   },
-]
+];
 
 const Navbar = () => {
   const pathname = usePathname();
-  
-  return ( 
+  console.log(pathname);
+  return (
     <nav className="flex gap-8">
       {links.map((link, index) => {
-        return ( 
-          <Link 
-            href = {link.path}  
-            key = {index} 
-            className = {'${link.path === pathname && "text-accent " }'} >
+        return (
+          <Link
+            href={link.path}
+            key={index}
+            className={`${
+              link.path === pathname
+                ? " capitalize text-accent border-b-2 border-accent"
+                : "capitalize font-medium hover:text-accent transition-all"
+            }`}
+          >
             {link.name}
           </Link>
-          );
+        );
       })}
-    </nav>  
+    </nav>
   );
 };
 
-export default Navbar
+export default Navbar;
